2e77bdd974b5db1ea2601fa66f04945119164c6f
Stefanc23 <stefano.chr23@gmail.com>
2021-03-10 16:26:12 +0700

    Add files for third lab session

---
+++ sesi3/src/com/stefano/ooplab/sesi3/Main.java
@@ -0,0 +1,32 @@
+package com.stefano.ooplab.sesi3;
+
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner input = new Scanner(System.in);
+	    Random rand = new Random();
+
+	    int magicNumber = rand.nextInt(101);
+	    int guess;
+
+        System.out.println("\nGuess a magic number between 0 and 100");
+	    while(true) {
+	    	System.out.print("\n\nEnter your guess: ");
+	    	try {
+				guess = input.nextInt();
+			}
+	    	catch(InputMismatchException e) {
+	    		input.nextLine();
+	    		continue;
+			}
+	    	if(guess < 0 || guess > 99) continue;
+	    	if(guess == magicNumber) break;
+	    	else System.out.println("Your guess is too " + ((guess > magicNumber) ? "high" : "low"));
+		}
+	    System.out.println("Yes, the number is " + guess);
+    }
+}
